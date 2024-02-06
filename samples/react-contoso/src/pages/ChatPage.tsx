c874e9d695d763337a7878105d13c196d347db61
andrewDoing <andrewsdoing@gmail.com>
2024-02-06 09:41:04 -0600

    Update react contoso

--- samples/react-contoso/src/pages/ChatPage.tsx
+++ samples/react-contoso/src/pages/ChatPage.tsx
@@ -85,8 +85,9 @@ const ChatListWrapper = memo(({ onSelected, onNewChat, selectedChatId }: ChatLis
   const onAllMessagesRead = useCallback((chatIds: string[]) => {
     console.log(`Number of chats marked as read: ${chatIds.length}`);
   }, []);
-  const onLoaded = useCallback(() => {
-    console.log('Chat threads loaded.');
+  const onLoaded = useCallback((num_chats: number) => {
+    console.log(num_chats, ' total chat threads loaded.');
+    return num_chats;
   }, []);
   const onMessageReceived = useCallback((msg: ChatMessage) => {
     console.log('SampleChatLog: Message received', msg);
